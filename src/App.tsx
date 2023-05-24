f83ad1404dc43ce4f8b325007f2251eed4ad1bb8
Christian Allen <7114281+crhallen@users.noreply.github.com>
2023-05-24 15:09:34 +0100

    Add todo list

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <ul>
+          <li>Test if its easy to install PWA</li>
+          <li>Test if app can get geolocation</li>
+          <li>Test if images etc can be saved offline</li>
+          <li>Test if upload can be done in the background</li>
+          <li>Test if its easy for user to get latest from PWA</li>
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
